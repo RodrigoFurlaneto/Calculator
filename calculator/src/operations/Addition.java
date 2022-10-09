dae6762e1a262db1dd3b3898459b7feb0e1c16f3
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-10-09 20:16:35 -0300

    Project calculator

---
+++ calculator/src/operations/Addition.java
@@ -0,0 +1,37 @@
+package operations;
+
+public class Addition {
+	
+	Double firstNumber;
+	Double secondNumber;
+	
+	public Addition() {
+		
+	}
+	
+	public Addition(Double firstNumber, Double secondNumber) {
+		this.firstNumber = firstNumber;
+		this.secondNumber = secondNumber;
+	}
+
+	public Double getFirstNumber() {
+		return firstNumber;
+	}
+
+	public void setFirstNumber(Double firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public Double getSecondNumber() {
+		return secondNumber;
+	}
+
+	public void setSecondNumber(Double secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+	
+	public Double result() {
+		return firstNumber + secondNumber;
+	}
+	
+}
